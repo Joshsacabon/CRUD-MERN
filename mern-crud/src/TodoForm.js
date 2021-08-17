9df4b5b466a04bee590ab036f44c484d16c8c2d3
Joshsacabon <sacabonj@gmail.com>
2021-08-17 14:33:57 +0800

    initial commit

---
+++ mern-crud/src/TodoForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useForm } from "react-hook-form";
+import { useHistory} from "react-router-dom";
+
+export const TodoForm = ({todo, onSubmit}) => {
+
+    const {register, handleSubmit,} = useForm();
+
+    const history = useHistory();
+
+    const submitHandler = handleSubmit((data) => {
+        onSubmit(data);
+        history.push("/");
+    });
+
+
+    return (
+        <form onSubmit={submitHandler}> 
+            <div className="form-group">
+                <label htmlFor="text">Text:</label>
+                <input className="form-control" {...register("test")}/>
+            </div>
+            <div className="form-group">
+                <button  type="submit" className="btn btn-primary ">
+                    CreateTodo
+                </button>
+            </div>
+        </form>
+)}
